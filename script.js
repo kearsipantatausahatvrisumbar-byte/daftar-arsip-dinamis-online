24be58232dd44edf5c067f158ef13cb44c0912e7
kearsipantatausahatvrisumbar-byte <kearsipantatausaha.tvrisumbar@gmail.com>
2026-01-07 10:56:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,53 @@
+let rowEdit = null;
+
+const fields = [
+  "noBerkas","kodeMaster","kodeKlasifikasi","indeks1","indeks2",
+  "nomorSurat","diberikanKepada","tanggalSurat","tingkatPerkembangan",
+  "jumlah","unitPengolah","retensiAktif","retensiInaktif","statusAkhir",
+  "noRak","noBox","noFolder","keterangan"
+];
+
 function simpanData() {
-  const fields = [
-    "noBerkas","kodeMaster","kodeKlasifikasi","indeks1","indeks2",
-    "nomorSurat","diberikanKepada","tanggalSurat","tingkatPerkembangan",
-    "jumlah","unitPengolah","retensiAktif","retensiInaktif","statusAkhir",
-    "noRak","noBox","noFolder","keterangan"
-  ];
+  if (rowEdit) {
+    updateData();
+    return;
+  }
 
   const tabel = document.getElementById("tabelData");
   const row = tabel.insertRow();
 
   fields.forEach((id, i) => {
-    const val = document.getElementById(id).value;
-    row.insertCell(i).innerText = val;
-    document.getElementById(id).value = "";
+    row.insertCell(i).innerText = document.getElementById(id).value;
   });
+
+  const aksi = row.insertCell(fields.length);
+  aksi.innerHTML = `<button onclick="editData(this)">Edit</button>`;
+
+  resetForm();
+}
+
+function editData(btn) {
+  rowEdit = btn.parentNode.parentNode;
+
+  fields.forEach((id, i) => {
+    document.getElementById(id).value = rowEdit.cells[i].innerText;
+  });
+
+  document.getElementById("btnSimpan").innerText = "Update Data";
+}
+
+function updateData() {
+  fields.forEach((id, i) => {
+    rowEdit.cells[i].innerText = document.getElementById(id).value;
+  });
+
+  rowEdit = null;
+  document.getElementById("btnSimpan").innerText = "Simpan Data";
+  resetForm();
+}
+
+function resetForm() {
+  fields.forEach(id => document.getElementById(id).value = "");
 }
 
 function exportCSV() {
@@ -28,9 +62,9 @@ function exportCSV() {
     csv.push(cols.join(","));
   }
 
-  const csvFile = new Blob([csv.join("\n")], { type: "text/csv" });
+  const blob = new Blob([csv.join("\n")], { type: "text/csv" });
   const link = document.createElement("a");
   link.download = "arsip_dinamis.csv";
-  link.href = URL.createObjectURL(csvFile);
+  link.href = URL.createObjectURL(blob);
   link.click();
 }
