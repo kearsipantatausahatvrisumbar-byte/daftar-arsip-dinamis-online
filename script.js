963b69a8def1049c2eefa063401f62ceb9299057
kearsipantatausahatvrisumbar-byte <kearsipantatausaha.tvrisumbar@gmail.com>
2026-01-08 09:33:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-// ambil elemen form dan tabel
 const form = document.getElementById("formArsip");
 const tabel = document.getElementById("tabelData");
 
-// ketika tombol simpan ditekan
 form.addEventListener("submit", function (e) {
-  e.preventDefault(); // mencegah refresh halaman
+  e.preventDefault();
 
-  // ambil nilai input
   const noBerkas = document.getElementById("noBerkas").value;
   const uraian = document.getElementById("uraian").value;
   const keterangan = document.getElementById("keterangan").value;
 
-  // buat baris baru
   const tr = document.createElement("tr");
 
   tr.innerHTML = `
@@ -20,9 +16,6 @@ form.addEventListener("submit", function (e) {
     <td>${keterangan}</td>
   `;
 
-  // masukkan ke tabel
   tabel.appendChild(tr);
-
-  // kosongkan form setelah simpan
   form.reset();
 });
