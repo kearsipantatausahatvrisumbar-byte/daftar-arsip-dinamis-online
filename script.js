981333fa47415b33fef7834de54d222e016bd261
kearsipantatausahatvrisumbar-byte <kearsipantatausaha.tvrisumbar@gmail.com>
2026-01-08 10:36:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,34 @@
-// ambil elemen
 const form = document.getElementById("formArsip");
 const tabel = document.getElementById("tabelData");
+
 const inputNoBerkas = document.getElementById("noBerkas");
 const inputUraian = document.getElementById("uraian");
 const inputIndeks1 = document.getElementById("indeks1");
 const inputKeterangan = document.getElementById("keterangan");
 
-// nomor berkas awal
+// nomor berkas otomatis
 let nomorBerkas = 1;
 inputNoBerkas.value = nomorBerkas;
 
-// event simpan
 form.addEventListener("submit", function (e) {
-  e.preventDefault(); // cegah reload
+  e.preventDefault();
 
   const noBerkas = inputNoBerkas.value;
   const uraian = inputUraian.value;
   const indeks1 = inputIndeks1.value;
   const keterangan = inputKeterangan.value;
 
-  // VALIDASI SEDERHANA
-  if (uraian === "") {
-    alert("Uraian Informasi wajib diisi");
-    return;
-  }
-
-  // buat baris tabel
   const tr = document.createElement("tr");
   tr.innerHTML = `
     <td>${noBerkas}</td>
     <td>${uraian}</td>
-    td>${indeks1}</td>
+    <td>${indeks1}</td>
     <td>${keterangan}</td>
   `;
 
-  // masukkan ke tabel
   tabel.appendChild(tr);
 
-  // reset input kecuali nomor
+  // reset input
   inputUraian.value = "";
   inputIndeks1.value = "";
   inputKeterangan.value = "";
@@ -46,4 +37,3 @@ form.addEventListener("submit", function (e) {
   nomorBerkas++;
   inputNoBerkas.value = nomorBerkas;
 });
-
